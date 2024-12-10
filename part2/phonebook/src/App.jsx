dd6b7061a0b98e68cefd6c9eb5440d2b7adbef01
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-12-10 22:43:01 +0700

    add db.json and integrate with app

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Filter from './Components/Filter'
 import Persons from './Components/Persons'
 import PersonForm from './Components/PersonForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [number, setNumber] = useState('')
   const [searchPersons, setSearchPersons] = useState('')
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then(res => {
+      const data = res.data
+      setPersons(data)
+    })
+  }, [])
   const addPerson = (e) => {
     e.preventDefault()
 
--- tem/tem/src/App.jsx
+++ tem/tem/src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Notes from './components/Notes'
-
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState(' a new static note...')
+import axios from 'axios'
+import noteServices from './services/notes'
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   // console.log(notes)
   const [showAll, setShowAll] = useState(true)
 
+  useEffect(() => {
+    noteServices.getAll().then(initialNote => setNotes(initialNote))
+  }, [])
 
   const noteToShow = showAll ? notes : notes.filter(note => note.important === true)
 
@@ -18,15 +22,29 @@ const App = (props) => {
     }
     const noteObject = {
       content: newNote,
-      important: false,
+      important: Math.random() > 0.5,
       id: String(notes.length + 1)
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+
+    noteServices.create(noteObject).then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
+      setNewNote('')
+    }
+    )
   }
 
   const handleNoteChange = (e) => {
     setNewNote(e.target.value)
+  }
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteServices
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id === id ? returnedNote : note))
+      })
   }
 
   return (
@@ -34,15 +52,21 @@ const App = (props) => {
       <h1>Notes</h1>
       <ul>
         {noteToShow.map(note =>
-          <Notes key={note.id} note={note} />
+          <Notes
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
         <button type='submit'>Add Note</button>
+
+
       </form>
       <button onClick={() => setShowAll(!showAll)}>
-        {showAll === true ? "show important" : "show all"}
+        {showAll ? "show important" : "show all"}
       </button>
     </div>
   )
