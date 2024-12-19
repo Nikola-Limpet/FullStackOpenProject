8a24641c244d3d0e7bcb6b81a5264062ec164e3d
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-12-19 14:04:22 +0700

    build static front

--- tem/api/server.js
+++ tem/api/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static('dist'))
 app.use(cors())
 
 let notes = [
