ee655060b6d3f3dda4e186a363f15139055349cd
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-12-29 17:46:23 +0700

    modify util and index

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -1,22 +1,34 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+mongoose.set('strictQuery', false)
 
-const Blog = mongoose.model('Blog', blogSchema)
+logger.info('connecting to', config.MONGO_DB_URI)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
 
+// Connect to DB
+mongoose.connect(config.MONGO_DB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+
+// Buildin MiddleWare
 app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
+app.use(middleware.requestLogger)
+
+
 
 app.get('/api/blogs', (request, response) => {
   Blog
@@ -36,7 +48,10 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+// custom error middlewares
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+
+
+module.exports = app
