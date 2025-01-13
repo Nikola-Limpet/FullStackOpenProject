ce574ae2ec72e401081deaabcc04ba4c519be8d1
Nikola Limpet <2023465lim@aupp.edu.kh>
2025-01-13 23:53:15 +0700

    done 5.1-5.4

---
+++ part5/blog-frontend/src/App.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
+import blogService from './services/blogs'
+import loginService from './services/login'
+import BlogForm from './components/BlogForm'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [notification, setNotification] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs(blogs)
+    )
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      setNotification({ message: 'Login successful', type: 'success' })
+      setTimeout(() => setNotification(null), 5000)
+    } catch (exception) {
+      setNotification({ message: 'Wrong username or password', type: 'error' })
+      setTimeout(() => setNotification(null), 5000)
+    }
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    setNotification({ message: 'Logged out', type: 'success' })
+    setTimeout(() => setNotification(null), 5000)
+  }
+
+  const addBlog = async (newBlog) => {
+    try {
+      const returnedBlog = await blogService.create(newBlog)
+      setBlogs(blogs.concat(returnedBlog))
+      setNotification({ message: `A new blog ${newBlog.title} by ${newBlog.author} added`, type: 'success' })
+      setTimeout(() => setNotification(null), 5000)
+    } catch (exception) {
+      setNotification({ message: 'Failed to add blog', type: 'error' })
+      setTimeout(() => setNotification(null), 5000)
+    }
+  }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {notification && <div className={notification.type}>{notification.message}</div>}
+      {user === null ? (
+        <LoginForm
+          handleLogin={handleLogin}
+          username={username}
+          password={password}
+          setUsername={setUsername}
+          setPassword={setPassword}
+        />
+      ) : (
+        <div>
+          <p>{user.username} logged in <button onClick={handleLogout}>logout</button></p>
+          <h2>create new</h2>
+          <BlogForm addBlog={addBlog} />
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
