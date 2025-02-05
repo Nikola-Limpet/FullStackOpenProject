ed25af106928bf341835b309a6be9c703a0ab247
Nikola Limpet <2023465lim@aupp.edu.kh>
2025-02-05 23:48:00 +0700

    init part9

---
+++ part9/exmaple/multiplier.ts
@@ -0,0 +1,22 @@
+
+const multiplicator = (a: number, b: number, op: Operation): Result => {
+  if (op === 'multiply') {
+    return a * b
+  } else if (op === 'add') {
+    return a + b
+  } else if (op === 'divide') {
+    if (b === 0) return 'can\'t divide 0!'
+    return a / b
+  }
+}
+
+
+multiplicator(1,2, 'divide')
+
+type Operation = 'multiply' | 'add' | 'divide';
+
+type Result = string | number;
+
+// const calculator = (a: number, b: number, op: Operation) : Result => {
+
+// }
